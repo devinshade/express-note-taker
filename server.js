1f188fcfc26052264288bafbfddbed5085cc0e96
devinshade <devindanielle93@gmail.com>
2023-12-06 08:42:47 -0700

    removed test notes

--- server.js
+++ server.js
@@ -28,28 +28,9 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './public/index.html'));
-// });
-
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.post('/api/notes', (req, res) => {
     allNotes().then(oldNotes => {
@@ -67,22 +48,6 @@ app.post('/api/notes', (req, res) => {
     // const newNote = createNewNote(req.body, allNotes);
     // res.json(newNote);
 });
-
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; ++i) {
-        let note = notesArray[i];
-
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
-
-            break;
-        }
-    }
-}
 
 app.delete('/api/notes/:id', (req, res) => {
     allNotes().then(oldNotes => {
